1f9f45a161830cdef5935f012b747b3194ef1f46
Victor <>
2021-09-22 15:55:33 +0200

    second commit

--- app/src/main/java/com/example/uppgift/MainActivity.java
+++ app/src/main/java/com/example/uppgift/MainActivity.java
@@ -18,15 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bt = (Button)findViewById(R.id.first_button);
-        TextView result = (TextView)findViewById(R.id.result_text);
-
-        EditText entryTal1 = (EditText)findViewById(R.id.first_number);
-        EditText entryTal2 = (EditText)findViewById(R.id.second_number);
 
 
+        Button bt = (Button) findViewById(R.id.first_button);
+        Button bt2 = (Button) findViewById(R.id.second_button);
+        TextView result = (TextView) findViewById(R.id.result_text);
 
-        View.OnClickListener listener = new View.OnClickListener(){
+        EditText entryTal1 = (EditText) findViewById(R.id.first_number);
+        EditText entryTal2 = (EditText) findViewById(R.id.second_number);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value = Integer.parseInt(entryTal1.getText().toString());
+                int value2 = Integer.parseInt(entryTal2.getText().toString());
+                result.setText("Result: " + String.valueOf(value + value2));
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value = Integer.parseInt(entryTal1.getText().toString());
+                int value2 = Integer.parseInt(entryTal2.getText().toString());
+                result.setText("Result: " + String.valueOf(value - value2));
+            }
+        });
+
+        /*View.OnClickListener listener = new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 int value = Integer.parseInt(entryTal1.getText().toString());
@@ -36,12 +55,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         bt.setOnClickListener(listener);
+    }*/
+
+
     }
-
-
-
-
-
-
-
 }
