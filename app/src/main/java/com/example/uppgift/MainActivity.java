f60908b98aa5e64aca7a023ad0976cfa7b6c5609
Victor <>
2021-09-22 15:32:28 +0200

    first commit

---
+++ app/src/main/java/com/example/uppgift/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.uppgift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt = (Button)findViewById(R.id.first_button);
+        TextView result = (TextView)findViewById(R.id.result_text);
+
+        EditText entryTal1 = (EditText)findViewById(R.id.first_number);
+        EditText entryTal2 = (EditText)findViewById(R.id.second_number);
+
+
+
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int value = Integer.parseInt(entryTal1.getText().toString());
+                int value2 = Integer.parseInt(entryTal2.getText().toString());
+                result.setText("Result: " + String.valueOf(value + value2));
+
+            }
+        };
+        bt.setOnClickListener(listener);
+    }
+
+
+
+
+
+
+
+}
